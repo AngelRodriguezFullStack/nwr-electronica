a77b151aa612ea81a8995ab0f4d9290a7513754a
angelprogramador <angelrodriguezcastro29@gmail.com>
2024-02-25 14:19:45 -0300

    mas js

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,10 @@
 let nombre = "angel";
 let apellido = "rodriguez"
-let anioDeNacimiento = 2001;
-let edad = 2024 - anioDeNacimiento;
-console.log();
+const ESPACIO = " "
+let edad = 2024;
+console.log(nombre + ESPACIO + apellido);
+
+let anioDeNacimiento = prompt("Ingrese año de nacimiento");
+
+let edadUsuario = 2024 - anioDeNacimiento;
+alert("tu edad es " + edadUsuario)
